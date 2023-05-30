fcd9f325ac43e6b8299325ab49f0dd64401ba29b
Daniel Sandoval <danielsand711@gmail.com>
2023-05-29 22:44:20 -0500

    photo update

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -215,50 +215,50 @@ export const portfolioItems: PortfolioItem[] = [
     image: porfolioImage3,
   },
   {
-    title: 'Project title 4',
-    description: 'Give .',
+    title: 'junkyard website',
+    description: 'website that provided general information and prices for materials received.  Included labor cost and directions via google maps link. .',
     url: '',
     image: porfolioImage4,
   },
   {
     title: 'Project title 5',
-    description: 'Give a short description of your project here.',
+    description: 'Give a short description .',
     url: 'ht',
     image: porfolioImage5,
   },
   {
     title: 'Project title 6',
-    description: 'Give a short description of your project here.',
+    description: 'Give a short descript.',
     url: 'http',
     image: porfolioImage6,
   },
   {
     title: 'Project title 7',
-    description: 'Give a short description of your project here.',
+    description: 'Give a short description.',
     url: 'htt',
     image: porfolioImage7,
   },
   {
     title: 'Project title 8',
-    description: 'Give a short description of your project here.',
+    description: 'Give a short description.',
     url: 'ht',
     image: porfolioImage8,
   },
   {
     title: 'Project title 9',
-    description: 'Give a short description of your project here.',
+    description: 'Give a short description .',
     url: 'htt',
     image: porfolioImage9,
   },
   {
     title: 'Project title 10',
-    description: 'Give a short description of your project here.',
+    description: 'Give a short description.',
     url: 'htt',
     image: porfolioImage10,
   },
   {
     title: 'Project title 11',
-    description: 'Give a short description of your project here.',
+    description: 'Give a short description.',
     url: 'htt',
     image: porfolioImage11,
   },
@@ -284,24 +284,24 @@ export const education: TimelineItem[] = [
 
 export const experience: TimelineItem[] = [
   {
-    date: 'March 2010 - Present',
-    location: 'Awesome Development Company',
+    date: 'March 2020 - Present',
+    location: 'Sandoval Bros',
     title: 'Senior UX Engineer',
     content: (
       <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
+        Describe 
+        anythi.
       </p>
     ),
   },
   {
-    date: 'March 2007 - February 2010',
-    location: 'Garage Startup Studio',
+    date: 'March 2015 February 2019',
+    location: 'home Startup Studio',
     title: 'Junior bug fixer',
     content: (
       <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
+        Describe work
+        anything.
       </p>
     ),
   },
@@ -314,18 +314,18 @@ export const testimonial: TestimonialSection = {
   imageSrc: testimonialImage,
   testimonials: [
     {
-      name: 'John Doe',
-      text: 'Use this as an opportunity to promote what it is like to work with you. High value testimonials include ones from current or past co-workers, managers, or from happy clients.',
+      name: 'Samuel parades',
+      text: 'Daniel helped set up my website for my junkyard guided me through possible layouts and explained what type of functions could be implemented.',
       image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/169.jpg',
     },
     {
-      name: 'Jane Doe',
-      text: 'Here you should write some nice things that someone has said about you. Encourage them to be specific and include important details (notes about a project you were on together, impressive quality produced, etc).',
+      name: 'Cynthia Rodriguez',
+      text: 'Here you should wrint.',
       image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/14.jpg',
     },
     {
-      name: 'Someone else',
-      text: 'Add several of these, and keep them as fresh as possible, but be sure to focus on quality testimonials with strong highlights of your skills/work ethic.',
+      name: 'S',
+      text: 'Add several of these,ills/work ethic.',
       image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/69.jpg',
     },
   ],
