d06d02d869d0cb47e1a9fc2a5e9fa9ad5d1b2921
dan s <Daniel_sandoval711@live.com>
2023-11-22 22:30:55 -0600

    fix

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -86,7 +86,12 @@ export const heroData: Hero = {
         </>
     ),
     actions: [
-        
+        {
+            href: 'assets/resume.pdf',
+            text: 'Resume',
+            primary: true,
+            Icon: ArrowDownTrayIcon,
+        },
         {
             href: `#${SectionId.Contact}`,
             text: 'Contact',
