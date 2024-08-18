ce5361e231f91b273b96be190fbff39f1d60f858
dansiff <danielsand711@gmail.com>
2024-08-17 22:23:33 -0500

    up

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -10,7 +10,6 @@ import {
 
 import GithubIcon from '../components/Icon/GithubIcon';
 import InstagramIcon from '../components/Icon/InstagramIcon';
-import LinkedInIcon from '../components/Icon/LinkedInIcon';
 import TwitterIcon from '../components/Icon/TwitterIcon';
 import heroImage from '../images/header-background.webp';
 import porfolioImage1 from '../images/portfolio/portfolio-1.jpg';
